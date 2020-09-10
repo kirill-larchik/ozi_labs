4c44a33f75b43379dc5dc9d0ba9ea996047389b0
Kirill Larchik <lar4ik.kirill@outlook.com>
2020-09-10 22:09:15 +0300

    Добавлены тесты.

--- lab02/UnitTests/PolynomMethodUT.cs
+++ lab02/UnitTests/PolynomMethodUT.cs
@@ -22,5 +22,41 @@ namespace UnitTests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Неверный формат последовательности чисел.")]
+        public void EncryptionAndDecryption_FirstException()
+        {
+            string expected = "ПРИКАЗ";
+
+            PolynomMethod polynomMethod = new PolynomMethod();
+
+            polynomMethod.N = 0;
+            polynomMethod.P = 991;
+
+            string encryptedMessage = polynomMethod.Encryption(expected);
+            string actual = polynomMethod.Decryption(encryptedMessage);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Неверный формат большого простого числа.")]
+        public void EncryptionAndDecryption_SecondException()
+        {
+            string expected = "ПРИКАЗ";
+
+            PolynomMethod polynomMethod = new PolynomMethod();
+
+            polynomMethod.N = 4;
+            polynomMethod.P = 0;
+
+            string encryptedMessage = polynomMethod.Encryption(expected);
+            string actual = polynomMethod.Decryption(encryptedMessage);
+
+            Assert.AreEqual(expected, actual);
+
+
+        }
     }
 }
